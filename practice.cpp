8da7d3b2ccb8a47a16654fb3a669f781628d406c
SreeshaNath <31390313+SreeshaNath@users.noreply.github.com>
2023-09-12 20:51:48 -0600

    Added practice.cpp
    This file includes a few functions for a lecture demo.

---
+++ practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int add(int a, int b);
+
+int multiply(int a, int b);
+
+bool isPrime(int a);
+
+bool isEven(int a);
+
+bool isOdd(int a);
+
+int main(int argc, char **argv){
+  //add a call to the function you are implementing
+  return EXIT_SUCCESS;
+}
